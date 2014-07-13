8ae73ad61ebd8534315fedf60e852492e5441bf1
Phạm Thế Nam <nampt_57@vnu.edu.vn>
2014-07-13 15:37:21 +0700

    first commit

--- src/test/java/oop/asg02/BigIntegerDemoTest.java
+++ src/test/java/oop/asg02/BigIntegerDemoTest.java
@@ -1,108 +1,105 @@
 package oop.asg02;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 import static org.junit.Assert.*;
 
 /**
- *
- * Unit test for simple BigIntegerDemo
+ * Unit test for BigIntegerDemo class.
  */
-public class BigIntegerDemoTest {
-    
-    public BigIntegerDemoTest() {
+public class BigIntegerDemoTest
+{
+    @Test
+    public void testCreateBigIntegerDemoFromInt()
+    {
+        BigIntegerDemo bigInt = new BigIntegerDemo(10);
+        assertEquals("10", bigInt.toString());
     }
     
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+    @Test
+    public void testCreateBigIntegerDemoFromStringWithLeadingZero()
+    {
+        BigIntegerDemo bigInt = new BigIntegerDemo("010");
+        assertEquals("10", bigInt.toString());
     }
 
-    /**
-     * Test of toString method, of class BigIntegerDemo.
-     */
     @Test
-    public void testToString() {
-        System.out.println("toString");
-        BigIntegerDemo instance = null;
-        String expResult = "";
-        String result = instance.toString();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testCreateBigIntegerDemoFromStringWithAllZeros()
+    {
+        BigIntegerDemo bigInt = new BigIntegerDemo("000");
+        assertEquals("0", bigInt.toString());
     }
 
-    /**
-     * Test of equals method, of class BigIntegerDemo.
-     */
     @Test
-    public void testEquals() {
-        System.out.println("equals");
-        Object other = null;
-        BigIntegerDemo instance = null;
-        boolean expResult = false;
-        boolean result = instance.equals(other);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testCreateBigIntegerDemoFromStringWithAllButUnitZeros()
+    {
+        BigIntegerDemo bigInt = new BigIntegerDemo("001");
+        assertEquals("1", bigInt.toString());
     }
 
-    /**
-     * Test of toLong method, of class BigIntegerDemo.
-     */
     @Test
-    public void testToLong() {
-        System.out.println("toLong");
-        BigIntegerDemo instance = null;
-        long expResult = 0L;
-        long result = instance.toLong();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of add method, of class BigIntegerDemo.
-     */
-    @Test
-    public void testAdd() {
-        System.out.println("add");
-        BigIntegerDemo other = null;
-        BigIntegerDemo instance = null;
-        BigIntegerDemo expResult = null;
-        BigIntegerDemo result = instance.add(other);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of subtract method, of class BigIntegerDemo.
-     */
-    @Test
-    public void testSubtract() {
-        System.out.println("subtract");
-        BigIntegerDemo other = null;
-        BigIntegerDemo instance = null;
-        BigIntegerDemo expResult = null;
-        BigIntegerDemo result = instance.subtract(other);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testConvertToLongWhenNotOverRange()
+    {
+        BigIntegerDemo bigInt = new BigIntegerDemo("10");
+        assertEquals(10L, bigInt.toLong());
     }
     
+    @Test
+    public void testEquality()
+    {
+        BigIntegerDemo bigInt1 = new BigIntegerDemo(10);
+        BigIntegerDemo bigInt2 = new BigIntegerDemo(10);
+        BigIntegerDemo bigInt3 = new BigIntegerDemo("10");
+                
+        assertEquals(bigInt1, bigInt2);
+        assertEquals(bigInt1, bigInt3);
+    }
+    
+    @Test
+    public void testInequality()
+    {
+        BigIntegerDemo bigInt1 = new BigIntegerDemo(10);
+        BigIntegerDemo bigInt2 = new BigIntegerDemo(12);
+                
+        assertFalse(bigInt1.equals(bigInt2));
+    }
+    
+    @Test
+    public void testAdditionWithoutCarryOn()
+    {
+        BigIntegerDemo bigInt1 = new BigIntegerDemo("111111111111111111111");
+        BigIntegerDemo bigInt2 = new BigIntegerDemo("1");
+        BigIntegerDemo sum = bigInt1.add(bigInt2);
+        
+        assertEquals(new BigIntegerDemo("111111111111111111112"), sum);
+    }
+   
+	@Test
+    public void testAdditionWithCarryOn()
+    {
+        BigIntegerDemo bigInt1 = new BigIntegerDemo("111111111111111111111");
+        BigIntegerDemo bigInt2 = new BigIntegerDemo("9");
+        BigIntegerDemo sum = bigInt1.add(bigInt2);
+        
+        assertEquals(new BigIntegerDemo("111111111111111111120"), sum);
+    }
+    
+    @Test
+    public void testSubtractionWithoutCarryOn()
+    {
+        BigIntegerDemo bigInt1 = new BigIntegerDemo("111111111111111111111");
+        BigIntegerDemo bigInt2 = new BigIntegerDemo("1");
+        BigIntegerDemo difference = bigInt1.subtract(bigInt2);
+        
+        assertEquals(new BigIntegerDemo("111111111111111111110"), difference);
+    }
+    
+    @Test
+    public void testSubtractionWithCarryOn()
+    {
+        BigIntegerDemo bigInt1 = new BigIntegerDemo("111111111111111111111");
+        BigIntegerDemo bigInt2 = new BigIntegerDemo("9");
+        BigIntegerDemo difference = bigInt1.subtract(bigInt2);
+        
+        assertEquals(new BigIntegerDemo("111111111111111111102"), difference);
+    }
 }
